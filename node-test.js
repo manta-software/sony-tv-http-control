3d5b440604acaaa04e11cfa8e36e99cb6fe0fc9e
Cuong Luu <cuongl@cakesolutions.net>
2017-09-01 22:19:36 +0100

    poc

--- node-test.js
+++ node-test.js
@@ -1,8 +1,8 @@
-const SonyTvHttpControl = require('./index');
+const SonyTvHttpControl = require('./dist/sony-tv-http-control');
 
 var sonyTvHttpControl = new SonyTvHttpControl('192.168.0.17');
 
-sonyTvHttpControl.requestControl('cuong', '5186')
+sonyTvHttpControl.requestControl('mac-pro', '3828')
     .then((response) => {
         console.log('Success', response);
         process.exit();
