f4804b3b506a2da779a142a5418928c4c9e1465b
Cuong Luu <cuongl@cakesolutions.net>
2017-09-03 23:03:26 +0100

    adding ssdp

--- src/sony-tv-http-control.js
+++ src/sony-tv-http-control.js
@@ -1,8 +1,16 @@
+import { URL } from 'url';
+import Promise from 'bluebird';
 import fs from 'fs';
 import request from 'request';
 import sonyTvCommands from './sony-tv-commands';
+import { Client as SsdpClient } from 'node-ssdp';
+import { parseString as parseXml } from 'xml2js';
 
 const payload = fs.readFileSync('resources/sony-ircc-payload.xml').toString('utf8');
+const SSDP_SERVICE_TYPE = 'urn:schemas-sony-com:service:IRCC:1';
+// const SSDP_SERVICE_TYPE = 'ssdp:all';
+
+const SSDP_DISCOVER_TIMEOUT = 10000;
 
 class SonyTvHttpControl {
 	constructor(host) {
@@ -58,6 +66,50 @@ class SonyTvHttpControl {
 					}
 				}).auth('', password);
 		});
+	}
+
+	discover(processDevice, timeout = SSDP_DISCOVER_TIMEOUT) {
+		const ssdp = new SsdpClient();
+		const timer = setTimeout(ssdp.stop, timeout);
+
+		ssdp.on('response', (headers, statusCode, data) => {
+			if (statusCode === 200) {
+				request.get(headers.LOCATION, (error, response, body) => {
+					if (!error && response.statusCode === 200) {
+						parseXml(body, (err, result) => {
+							if (!err) {
+								const device = result.root.device[0];
+								const service = device.serviceList[0].service
+									.find(service => service.serviceType[0] === SSDP_SERVICE_TYPE);
+
+								const api = new URL(service.controlURL[0]);
+								console.log('processing', api);
+								processDevice({
+									host: api.host,
+									port: (api.port || 80),
+									friendlyName: device.friendlyName[0],
+									manufacturer: device.manufacturer[0],
+									manufacturerURL: device.manufacturerURL[0],
+									modelName: device.modelName[0],
+									UDN: device.UDN[0]
+								})
+									.then(() => ssdp.stop())
+									.then(() => clearTimeout(timer))
+									.catch((err) => {
+										console.warn(`Failed to process: ${api}`, err);
+									});
+							} else {
+								console.warn(`Failed to parse the response: ${err}.`);
+							}
+						});
+					} else {
+						console.warn(`Error for device ${data.address}, statusCode: ${response.statusCode}, error: ${error}, body: ${body}`);
+					}
+				});
+			}
+		});
+
+		ssdp.search(SSDP_SERVICE_TYPE);
 	}
 
 	powerOn() {
