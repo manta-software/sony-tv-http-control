2b2342b3d72f27de200f230e4c67451827f8dcde
Cuong Luu <cuongl@cakesolutions.net>
2017-09-10 22:49:54 +0100

    adding cli

--- src/sony-tv-http-control.js
+++ src/sony-tv-http-control.js
@@ -5,14 +5,15 @@ import request from 'request';
 import sonyTvCommands from './sony-tv-commands';
 import { Client as SsdpClient } from 'node-ssdp';
 import { parseString as parseXml } from 'xml2js';
+import { minutes } from './utils/units';
 
 const payload = fs.readFileSync('resources/sony-ircc-payload.xml').toString('utf8');
 const SSDP_SERVICE_TYPE = 'urn:schemas-sony-com:service:IRCC:1';
 // const SSDP_SERVICE_TYPE = 'ssdp:all';
 
-const SSDP_DISCOVER_TIMEOUT = 10000;
+const SSDP_DISCOVER_DEFAULT_TIMEOUT = minutes(1);
 
-class SonyTvHttpControl {
+export class SonyTvHttpControl {
 	constructor(host) {
 		this.host = host;
 
@@ -68,22 +69,32 @@ class SonyTvHttpControl {
 		});
 	}
 
-	discover(processDevice, timeout = SSDP_DISCOVER_TIMEOUT) {
-		const ssdp = new SsdpClient();
-		const timer = setTimeout(ssdp.stop, timeout);
+	powerOn() {
+		return this.sendCommand('WakeUp');
+	}
 
-		ssdp.on('response', (headers, statusCode, data) => {
-			if (statusCode === 200) {
-				request.get(headers.LOCATION, (error, response, body) => {
-					if (!error && response.statusCode === 200) {
-						parseXml(body, (err, result) => {
-							if (!err) {
-								const device = result.root.device[0];
-								const service = device.serviceList[0].service
-									.find(service => service.serviceType[0] === SSDP_SERVICE_TYPE);
+	powerOff() {
+		return this.sendCommand('PowerOff');
+	}
+}
 
+export function discover(processDevice, timeout = SSDP_DISCOVER_DEFAULT_TIMEOUT) {
+	const ssdp = new SsdpClient();
+	const timer = setTimeout(ssdp.stop, timeout);
+
+	ssdp.on('response', (headers, statusCode, data) => {
+		if (statusCode === 200) {
+			request.get(headers.LOCATION, (error, response, body) => {
+				if (!error && response.statusCode === 200) {
+					parseXml(body, (err, result) => {
+						if (!err) {
+							const device = result.root.device[0];
+							const service = device.serviceList[0].service
+								.find(service => service.serviceType[0] === SSDP_SERVICE_TYPE);
+
+							if (service) {
 								const api = new URL(service.controlURL[0]);
-								console.log('processing', api);
+
 								processDevice({
 									host: api.host,
 									port: (api.port || 80),
@@ -98,27 +109,17 @@ class SonyTvHttpControl {
 									.catch((err) => {
 										console.warn(`Failed to process: ${api}`, err);
 									});
-							} else {
-								console.warn(`Failed to parse the response: ${err}.`);
 							}
-						});
-					} else {
-						console.warn(`Error for device ${data.address}, statusCode: ${response.statusCode}, error: ${error}, body: ${body}`);
-					}
-				});
-			}
-		});
+						} else {
+							console.warn(`Failed to parse the response: ${err}.`);
+						}
+					});
+				} else {
+					console.warn(`Error for device ${data.address}, statusCode: ${response.statusCode}, error: ${error}, body: ${body}`);
+				}
+			});
+		}
+	});
 
-		ssdp.search(SSDP_SERVICE_TYPE);
-	}
-
-	powerOn() {
-		return this.sendCommand('WakeUp');
-	}
-
-	powerOff() {
-		return this.sendCommand('PowerOff');
-	}
+	ssdp.search(SSDP_SERVICE_TYPE);
 }
-
-module.exports = SonyTvHttpControl;
---
+++ src/utils/units.js
@@ -0,0 +1,3 @@
+const MILLIS = 1000;
+
+export const minutes = (value) => value * 60 * MILLIS;
