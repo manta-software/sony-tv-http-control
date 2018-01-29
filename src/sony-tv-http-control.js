db32d25e17405bf392de1270cf08c8c090a90dc1
Cuong Luu <cuongl@cakesolutions.net>
2018-01-29 22:33:01 +0000

    finished poc with unit tests and cli

--- src/sony-tv-http-control.js
+++ src/sony-tv-http-control.js
@@ -1,154 +1,76 @@
-import {URL} from 'url';
-import Promise from 'bluebird';
 import request from 'request';
-import sonyTvCommands from './sony-tv-commands';
-import {Client as SsdpClient} from 'node-ssdp';
-import {parseString as parseXml} from 'xml2js';
-import {minutes} from './utils/units';
+import {getPayload, getAccessControlPayload} from './utils/sony-tv-request-payload';
 
-const payload =
-    `<?xml version="1.0"?>
-    <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-      <s:Body>
-        <u:X_SendIRCC xmlns:u="urn:schemas-sony-com:service:IRCC:1">
-          <IRCCCode>{{commandCode}}</IRCCCode>
-        </u:X_SendIRCC>
-      </s:Body>
-    </s:Envelope>`;
-const SSDP_SERVICE_TYPE = 'urn:schemas-sony-com:service:IRCC:1';
-// const SSDP_SERVICE_TYPE = 'ssdp:all';
-
-const SSDP_DISCOVER_DEFAULT_TIMEOUT = minutes(1);
-
-export class SonyTvHttpControl {
-  constructor(host) {
-    this.host = host;
-
-    this.baseRequest = request.defaults({
-      baseUrl: `http://${host}/sony`,
+function sendAccessControl(host, clientId, passcode) {
+  return new Promise((resolve, reject) => {
+    const opts = {
+      url: `http://${host}/sony/accessControl`,
       method: 'post',
+      json: true,
+      body: getAccessControlPayload(clientId),
+      sendImmediately: false
+    };
+
+    request(opts, (error, response) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(response);
+      }
+    }).auth('', passcode);
+  });
+}
+
+export const requestAccessControl = (host, clientId) => {
+  return sendAccessControl(host, clientId);
+};
+
+export const sendAuthorisation = (host, clientId, passcode) => {
+  return sendAccessControl(host, clientId, passcode)
+    .then((response) => {
+      return response.headers['set-cookie'][0];
+    });
+};
+
+export const sendSystemCommand = (host) => {
+  return new Promise((resolve, reject) => {
+    const opts = {
+      url: `http://${host}/sony/system`,
+      method: 'post',
+      body: '{"id":20,"method":"getRemoteControllerInfo","version":"1.0","params":[]}'
+    };
+
+    request(opts, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body));
+      }
+    });
+  });
+};
+
+export const sendCommand = (host, commandCode, cookies) => {
+  return new Promise((resolve, reject) => {
+    const commandPayload = getPayload(commandCode);
+
+    const opts = {
+      url: `http://${host}/sony/IRCC`,
+      method: 'post',
+      body: commandPayload,
       headers: {
         'Content-Type': 'text/xml',
         'SOAPAction': '"urn:schemas-sony-com:service:IRCC:1#X_SendIRCC"',
-      },
-    });
-  }
-
-  sendCommand(commandName) {
-    const commandCode = sonyTvCommands[commandName];
-
-    return new Promise((resolve, reject) => {
-      if (!commandCode) {
-        reject(`unknown command: ${commandName}`);
+        'Cookie': cookies // Cookie header is case-sensitive, otherwise Sony TV will reject it
       }
+    };
 
-      const commandPayload = payload.replace('{{commandCode}}', commandCode);
-
-      const opts = {
-        body: commandPayload,
-        uri: '/IRCC',
-      };
-
-      this.baseRequest(opts, (error, response, body) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(body);
-        }
-      });
+    request(opts, (error) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve('OK');
+      }
     });
-  }
-
-  requestControl(clientId, password) {
-    return new Promise((resolve, reject) => {
-      request({
-        url: `http://${this.host}/sony/accessControl`,
-        method: 'post',
-        body: `{"id":13,"method":"actRegister","version":"1.0","params":[{"clientid":"${clientId}:1","nickname":"${clientId}"},[{"clientid":"${clientId}:1","value":"yes","nickname":"${clientId}","function":"WOL"}]]}`,
-        sendImmediately: false,
-      }, (error, response, body) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(body);
-        }
-      }).auth('', password);
-    });
-  }
-
-  powerOn() {
-    return this.sendCommand('WakeUp');
-  }
-
-  powerOff() {
-    return this.sendCommand('PowerOff');
-  }
-
-  sendSystemCommand() {
-    console.log('sendSystemCommand');
-    return new Promise((resolve, reject) => {
-      const opts = {
-        url: `http://${this.host}/sony/system`,
-        method: 'post',
-        body: '{"id":20,"method":"getRemoteControllerInfo","version":"1.0","params":[]}',
-      };
-
-      request(opts, (error, response, body) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(JSON.parse(body));
-        }
-      });
-    });
-  }
-}
-
-export function discover(
-    processDevice, timeout = SSDP_DISCOVER_DEFAULT_TIMEOUT) {
-  const ssdp = new SsdpClient();
-  const timer = setTimeout(() => {
-    ssdp.stop();
-  }, timeout);
-
-  ssdp.on('response', (headers, statusCode, data) => {
-    if (statusCode === 200) {
-      request.get(headers.LOCATION, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-          parseXml(body, (err, result) => {
-            if (!err) {
-              const device = result.root.device[0];
-              const service = device.serviceList[0].service.find((service) => service.serviceType[0] === SSDP_SERVICE_TYPE);
-
-              if (service) {
-                const api = new URL(service.controlURL[0]);
-
-                processDevice({
-                  host: api.host,
-                  port: (api.port || 80),
-                  friendlyName: device.friendlyName[0],
-                  manufacturer: device.manufacturer[0],
-                  manufacturerURL: device.manufacturerURL[0],
-                  modelName: device.modelName[0],
-                  UDN: device.UDN[0],
-                }).
-                    then(() => ssdp.stop()).
-                    then(() => clearTimeout(timer)).
-                    catch((err) => {
-                      console.warn(
-                          `Failed to process: ${device.friendlyName[0]}`, err);
-                    });
-              }
-            } else {
-              console.warn(`Failed to parse the response: ${err}.`);
-            }
-          });
-        } else {
-          console.warn(`Error for device ${data.address}, statusCode: ${response.statusCode}, error: ${error}, body: ${body}`);
-        }
-      });
-    }
   });
-
-  ssdp.search(SSDP_SERVICE_TYPE);
-}
+};
